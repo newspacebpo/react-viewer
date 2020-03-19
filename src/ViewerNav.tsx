ff9be0dbb98a1da73412f50717739f39f7d66061
Ciro <ciro.dorsa@cadmus.com.br>
2020-03-19 16:01:17 -0300

    Mais um teste

--- src/ViewerNav.tsx
+++ src/ViewerNav.tsx
@@ -13,7 +13,6 @@ export interface ViewerNavProps {
 export default class ViewerNav extends React.Component<ViewerNavProps, any> {
   static defaultProps = {
     activeIndex: 0,
-    showToggleNav: true,
   };
   constructor(props) {
     super(props);
